19e555c5b23d8eaae360233826bfb1eab958e2a5
chelydra-ua <lipara.shep@gmail.com>
2022-07-16 23:49:21 +0300

    script animation added

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ window.onload = function () {
     let btn = document.getElementById('jump');
     let count = 0;
 
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    let x = 300;
+    let y = 350;
+
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+
     btn.onclick = function () {
         count += 1;
+        y -= 25;
+        context.clearRect(0, 0, 600, 400);
+
+        context.beginPath();
+        context.arc(x, y, 50, 0, 2 * Math.PI);
+        context.fillStyle = 'red';
+        context.fill();
+
+        context.font = '25px Arial';
+        context.fillStyle = 'white';
+        context.fillText('Count: ' + count, 20, 30);
     };
 };
-
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const x = 300;
-const y = 350;
-
-context.arc(x, y, 50, 0, 2 * Math.PI);
-context.fillStyle = 'red';
-context.fill();
