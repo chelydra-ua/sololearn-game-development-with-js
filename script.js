e1ec97b3dbe281f724a4dc2becc82859a557974e
chelydra-ua <lipara.shep@gmail.com>
2022-07-16 23:16:27 +0300

    dravning on canvas added

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-window.onload = function() {
-    let btn = document.getElementById("jump");
+window.onload = function () {
+    let btn = document.getElementById('jump');
     let count = 0;
 
-    btn.onclick = function() {
-        count +=1
-    }
-}
+    btn.onclick = function () {
+        count += 1;
+    };
+};
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const x = 300;
+const y = 350;
+
+context.arc(x, y, 50, 0, 2 * Math.PI);
+context.fillStyle = 'red';
+context.fill();
