35789ca866849c91861d701267e71548189f752b
chelydra-ua <lipara.shep@gmail.com>
2022-07-17 17:35:02 +0300

    js fixed

--- script.js
+++ script.js
@@ -14,15 +14,18 @@ window.onload = function () {
     btn.onclick = function () {
         count += 1;
         y -= 25;
-        context.clearRect(0, 0, 600, 400);
+        function draw() {
+            context.clearRect(0, 0, 600, 400);
 
-        context.beginPath();
-        context.arc(x, y, 50, 0, 2 * Math.PI);
-        context.fillStyle = 'red';
-        context.fill();
+            context.beginPath();
+            context.arc(x, y, 50, 0, 2 * Math.PI);
+            context.fillStyle = 'red';
+            context.fill();
 
-        context.font = '25px Arial';
-        context.fillStyle = 'white';
-        context.fillText('Count: ' + count, 20, 30);
+            context.font = '25px Arial';
+            context.fillStyle = 'white';
+            context.fillText('Count: ' + count, 20, 30);
+        }
+        window.requestAnimationFrame(draw);
     };
 };
